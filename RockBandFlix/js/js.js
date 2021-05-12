57fe30a11aaa1dd457fde73fd1c5241344b4ab64
David <davidguittar25@gmail.com>
2021-05-12 01:35:21 -0300

    recriando interface netflix

---
+++ RockBandFlix/js/js.js
@@ -0,0 +1,133 @@
+function mudar1(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto1-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto1-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";          
+}
+
+function mudar2(){
+   
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto2-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+
+      foto2.src="img/foto2-esq.jpg";
+      foto2.style.width = "680px";
+      foto2.style.height = "400px";
+      
+    
+}
+
+function mudar3(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto3-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto3-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";  
+}
+
+function mudar4(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto4-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto4-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";  
+}
+
+function mudar5(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto5-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto5-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";  
+}
+
+function mudar6(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto6-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto6-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";  
+}
+
+function mudar7(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto7-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto7-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";  
+}
+
+function mudar8(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto8-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto8-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";
+}
+
+function mudar9(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto9-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto9-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";  
+}
+
+function mudar10(){
+    let foto1 = document.getElementById("1-d")
+    let foto2 = document.getElementById('1-e')
+
+    foto1.src="img/foto10-dir.jpg";
+    foto1.style.width = "680px";
+    foto1.style.height = "400px";
+
+    foto2.src="img/foto10-esq.jpg";
+    foto2.style.width = "680px";
+    foto2.style.height = "400px";
+}
